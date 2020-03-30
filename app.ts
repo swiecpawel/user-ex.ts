66ebf025307c0758f4d90c7a1ccacb9aeeeac19f
Pawel <swiecpawel@gmail.com>
2020-03-31 01:53:13 +0200

    Change app.ts

--- app.ts
+++ app.ts
@@ -1,22 +1,27 @@
 let elem = document.querySelector('#table-body')! as HTMLElement;
 const companiesSite = "http://localhost:4000/companies";
 const usersSite = "http://localhost:4000/users";
-let table:string ="";
-let units:number = 0; //start unit
-const sortedCompanies = loadAll();
+let table = "";
+let units = 0 ; //start unit
+const sortedCompanies: Promise<Companies> = loadAll();
 sortedCompanies.then(el => renderCompany(el, units));
+
 async function loadAll() {
-    const user            = await fetch(usersSite).then(res => res.json());
-    const companies       = await fetch(companiesSite).then(res => res.json());
-    const sortedCompanies = await sortCompanyById(user,companies);
-    return sortedCompanies;
+    const user: Users          = await fetch(usersSite).then(res => res.json());
+    const companies: Companies = await fetch(companiesSite).then(res => res.json());
+
+    return sortCompanyById(user,companies);
 }
-function sortCompanyById(users, companies){
-    let uri:string;
-    let sortedCompaniesAddedUsers:Array<object>;
+
+type Users =  Array<{name:string, email: string, uris: {company: string}}>;
+type Companies = Array< {name: string, uri: string,  usersAmount?: number, users?: Users}>;
+
+function sortCompanyById(users: Users, companies: Companies ): Companies{
+    let uri;
+    let sortedCompaniesAddedUsers;
     companies.map(company => {
         company.usersAmount = 0;
-        company.users = {};
+        company.users;
     });
     companies.forEach( company => {
         uri = company.uri;
@@ -27,7 +32,8 @@ function sortCompanyById(users, companies){
     companies.sort((a, b) => a.usersAmount - b.usersAmount);
     return companies;
 }
-function renderCompany(srtCompanies, limit) {
+
+function renderCompany(srtCompanies: Companies, limit: number): void {
     for (let i = limit; i < limit + 10; i++) {
         let company = srtCompanies[i];
         table += `<tr id="${company.uri}"><td>${company.name}</td><td class="td_weight">${company.usersAmount}</td>
@@ -41,7 +47,7 @@ function renderCompany(srtCompanies, limit) {
     }
     elem.insertAdjacentHTML('beforeend', table)
 }
-function nextPage(){
+function nextPage(): void{
     if(units < 989) {
         units += 10;
         table = "";
@@ -49,7 +55,7 @@ function nextPage(){
         sortedCompanies.then(el => renderCompany(el, units))
     }
 }
-function prevPage() {
+function prevPage(): void{
     if(units > 9){
         units -= 10;
         table = "";
@@ -57,17 +63,17 @@ function prevPage() {
         sortedCompanies.then(el => renderCompany(el, units))
     }
 }
-function showUsers(uri, users) {
-    let displayedCompany = document.getElementById(`${uri}`).nextSibling;
+function showUsers(uri: string, users: number) {
+    let displayedCompany = (document.getElementById(`${uri}`)! as HTMLElement).nextElementSibling;
     if (users > 0) {
-        if (displayedCompany.style.display === 'none')
+        if (displayedCompany instanceof HTMLElement && displayedCompany.style.display === 'none')
             for(let i=0; i < users; i++){
-                displayedCompany.style.display = '';
-                displayedCompany = displayedCompany.nextSibling;
+                displayedCompany.setAttribute("style", "display:''");
+                displayedCompany = displayedCompany.nextElementSibling;
             } else {
             for(let i=0; i < users; i++){
-                displayedCompany.style.display = 'none';
-                displayedCompany = displayedCompany.nextSibling;
+                displayedCompany.setAttribute("style", "display:none");
+                displayedCompany = displayedCompany.nextElementSibling;
             }
         }
     }
